89c4d37b85d1b668c111e667eadfe9ffe2400918
Zee1234 <zeeatgit@gmail.com>
2017-09-10 08:41:27 -0500

    Some new tests

--- spec/busted_spec.lua
+++ spec/busted_spec.lua
@@ -1,9 +1,10 @@
 local List = require('list')
 
-local function genericGenerator(_, d) return d end
+local function genericGenerator(_, d) return d end -- Identical to List's default generator
+local function gen(d) return d end -- Returns all data to use as key, good for testing
 
-describe('New instances forming', function()
-  it('checks that new instances are identical, but not equal', function()
+describe('New instances', function()
+  it('are checked to be identical, but not equal', function()
     assert.are.same(List.new(genericGenerator), List.new(genericGenerator), {
       _store = {
         generator = genericGenerator;
@@ -12,15 +13,49 @@ describe('New instances forming', function()
         pairs = {};
       }
     })
+
     assert.are.unique(List.new(genericGenerator), List.new(genericGenerator))
   end)
+end)
 
-  it('checks that (de)population occurs correctly', function()
-    local function gen(d) return d end
+
+describe('With new instances,', function()
+  it('checks that (de)population occurs identically', function()
     local listA = List.new(gen):write('1'):write('2'):write('3')
     local listB = List.new(gen):write('1'):write('2'):write('3')
     assert.are.same(listA, listB)
 
     assert.are.same(listA:remove('1'), listB:remove('1'))
+  end)
+end)
+
+describe('Chained methods', function()
+  it('continue to function if no error', function()
+    assert.is.truthy(
+      List.new()
+          :write('a')
+          :write('b')
+          :write('c')
+          :write('d')
+          :remove('d')
+          :remove(3)
+          :write('c')
+          :write('d')
+    )
+  end)
+
+  it('insert into removed spaces properly', function()
+    local listA = List.new(gen)
+                      :write('a')
+                      :write('b')
+                      :write('c')
+                      :write('d')
+                      :remove('b')
+                      :remove('c')
+                      :write('e')
+    local store = listA._store
+    assert.are.same(listA, {
+      'a', 'e', nil, 'd', _store = store
+    })
   end)
 end)
