a717dd64d6b2b248e192d76b6ead8a3d8f5f0de1
Zee1234 <zeeatgit@gmail.com>
2017-09-11 09:15:42 -0500

    More tests to test

--- spec/busted_spec.lua
+++ spec/busted_spec.lua
@@ -3,6 +3,13 @@ local List = require('list')
 local function genericGenerator(_, d) return d end -- Identical to List's default generator
 local function gen(d) return d end -- Returns all data to use as key, good for testing
 
+local function random
+math.randomseed(os.clock*1000)
+math.random()
+math.random()
+math.random()
+
+
 describe('New instances', function()
   it('are checked to be identical, but not equal', function()
     assert.are.same(List.new(genericGenerator), List.new(genericGenerator), {
@@ -19,6 +26,9 @@ describe('New instances', function()
 end)
 
 
+
+
+
 describe('With new instances,', function()
   it('checks that (de)population occurs identically', function()
     local listA = List.new(gen):write('1'):write('2'):write('3')
@@ -28,6 +38,10 @@ describe('With new instances,', function()
     assert.are.same(listA:remove('1'), listB:remove('1'))
   end)
 end)
+
+
+
+
 
 describe('Chained methods', function()
   it('continue to function if no error', function()
@@ -43,6 +57,8 @@ describe('Chained methods', function()
           :write('d')
     )
   end)
+
+
 
   it('insert into removed spaces properly', function()
     local listA = List.new(gen)
@@ -61,7 +77,6 @@ describe('Chained methods', function()
 end)
 
 describe('`removeReturn`', function()
-
   it('removes properly',function() 
     local listA = List.new(gen):write('a')
     local ret = listA:removeReturn('a')
@@ -70,6 +85,8 @@ describe('`removeReturn`', function()
       _store = store
     })
   end)
+
+
 
   it('returns properly', function()
     local listA = List.new(gen):write('a')
@@ -85,5 +102,115 @@ describe('`removeReturn`', function()
 
     assert.are.equals(ret, arbitrary)
     assert.are.same(listA, listB)
+  end)
+end)
+
+describe('`getIndex`', function()
+  it('returns index correctly', function()
+    local listA = List.new(gen)
+                      :write('a')
+                      :write('b')
+                      :write('c')
+                      :write('d')
+                      :remove('b')
+                      :remove('c')
+                      :write('e')
+    
+    assert.is.true(listA:getIndex('e') == 2)
+    assert.is.true(listA:getIndex('b') == nil)
+  end)
+end)
+
+
+
+
+
+describe('Iteration:', function()
+  describe('`iter`:', function()
+    it('returns next element', function()
+      local vals = {'a', 'b', 'c', 'd', 'e'}
+      local listA = List.new(gen)
+                        :write(vals[1])
+                        :write(vals[2])
+                        :write(vals[3])
+                        :write(vals[4])
+                        :remove(vals[2])
+                        :remove(vals[3])
+                        :write(vals[5])
+      
+      for index, value, id in listA.iter, listA, 0 do
+        local i = 1
+        assert.are.equals(listA[index], value, listA[id], id, listA[i], vals[1])
+        break;
+      end
+      for index, value, id in listA.iter, listA, 1 do
+        local i = 2
+        assert.are.equals(listA[index], value, listA[id], id, listA[i], vals[5])
+        break;
+      end
+      for index, value, id in listA.iter, listA, 2 do
+        local i = 4
+        assert.are.equals(listA[index], value, listA[id], id, listA[i], vals[4])
+        break;
+      end
+    end)
+
+
+
+    it('is returned by `iterate`', function()
+      local listA = List.new()
+      local iter, tab = List:iterate()
+
+      assert.are.same(iter, List.iter)
+    end)
+  end)
+
+
+  describe('`iterate`', function()
+    it('properly iterates over straight writes', function()
+      local listA = List.new(gen)
+      local vals = {}
+      for i=65, 122, 1 do
+        vals.insert(string.char(i)..tostring(i-64))
+        listA:write(vals[#vals])
+      end
+      
+      for index, value, id in listA:iterate() do
+        assert.are.same(listA[index], listA[id], value, vals[index])
+      end
+    end)
+
+
+
+    it('properly iterates over skipped values', function()
+      local listA = List.new(gen)
+      local vals = {}
+      for i=65, 122, 1 do
+        table.insert(vals, string.char(i)..tostring(i-64))
+        listA:write(vals[#vals])
+      end
+
+      local removes = {}
+      for i=1, 30, 1 do
+        if removes[tostring(i)] then continue; end
+        removes[tostring(i)] = true
+        table.insert(removes, i)
+      end
+
+      for _, v in ipairs(removes) do
+        listA:remove(v)
+      end
+
+      local keptVals, listVals = {}, {}
+      for i, v in ipairs(vals) do
+        if removes[tostring(i)] then continue; end
+        table.insert(keptVals, v)
+      end
+      for index, val, id in listA:iterate() do
+        table.insert(listVals, val)
+      end
+
+      assert.are.same(listVals, keptVals)
+    end)
   end)
 end)
